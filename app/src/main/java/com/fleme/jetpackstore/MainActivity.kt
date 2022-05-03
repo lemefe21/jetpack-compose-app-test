f14634338e50af0712b64ec7f910fcf9982ffb17
Felipe Leme <lemefe21@gmail.com>
2022-05-03 15:39:11 -0300

    [MAIN] creating first component with compose

--- app/src/main/java/com/fleme/jetpackstore/MainActivity.kt
+++ app/src/main/java/com/fleme/jetpackstore/MainActivity.kt
@@ -3,11 +3,21 @@ package com.fleme.jetpackstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fleme.jetpackstore.ui.theme.JetpackStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +27,7 @@ class MainActivity : ComponentActivity() {
             JetpackStoreTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CardItem("Android")
                 }
             }
         }
@@ -25,14 +35,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CardItem(name: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.icon_jetpack_compose),
+            contentDescription = "Item",
+            modifier = Modifier.size(56.dp).padding(end = 2.dp)
+        )
+        Column {
+            Text(
+                text = "Hello $name!"
+            )
+            Text(
+                text = "Welcome to Jetpack Store.",
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackStoreTheme {
-        Greeting("Android")
+        CardItem("Android")
     }
 }

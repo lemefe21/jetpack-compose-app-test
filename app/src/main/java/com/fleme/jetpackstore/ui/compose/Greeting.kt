2db2ef066d635aeea5d0c472088920ac959f9ba3
Felipe Leme <lemefe21@gmail.com>
2022-05-19 15:33:28 -0300

    [MAIN] use rememberSaveable for save state on configuration changes or process death

--- app/src/main/java/com/fleme/jetpackstore/ui/compose/Greeting.kt
+++ app/src/main/java/com/fleme/jetpackstore/ui/compose/Greeting.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -81,7 +82,7 @@ fun PreviewGreetingList() {
 
 @Composable
 fun AppLaunchGreeting() {
-    var shouldShowOnboardingScreen by remember { mutableStateOf(true) }
+    var shouldShowOnboardingScreen by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboardingScreen) {
         OnboardingScreen(onContinueClicked = {shouldShowOnboardingScreen = false})

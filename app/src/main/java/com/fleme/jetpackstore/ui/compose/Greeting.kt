7d23e00b1552f11c79c0badf1817eca8f235123d
Felipe Leme <lemefe21@gmail.com>
2022-05-19 11:39:16 -0300

    [MAIN] create onboarding screen and refactor logic to hoisted state

---
+++ app/src/main/java/com/fleme/jetpackstore/ui/compose/Greeting.kt
@@ -0,0 +1,99 @@
+package com.fleme.jetpackstore.ui.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fleme.jetpackstore.ui.theme.JetpackStoreTheme
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun PreviewGreeting() {
+    JetpackStoreTheme {
+        Greeting(name = "Androd")
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String> = listOf("Jetpack", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320
+)
+@Composable
+fun PreviewGreetingList() {
+    JetpackStoreTheme {
+        GreetingList()
+    }
+}
+
+@Composable
+fun AppLaunchGreeting() {
+    var shouldShowOnboardingScreen by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboardingScreen) {
+        OnboardingScreen(onContinueClicked = {shouldShowOnboardingScreen = false})
+    } else {
+        GreetingList()
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun PreviewAppLaunchGreeting() {
+    JetpackStoreTheme {
+        AppLaunchGreeting()
+    }
+}
---
+++ app/src/main/java/com/fleme/jetpackstore/ui/compose/OnboardingScreen.kt
@@ -0,0 +1,55 @@
+package com.fleme.jetpackstore.ui.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fleme.jetpackstore.ui.theme.JetpackStoreTheme
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit
+) {
+    // This state should be hoisted
+    // var shouldShowOnboardingScreen by remember { mutableStateOf(true) }
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Jetpack Store!")
+            Button(
+                modifier = Modifier.padding(24.dp),
+                onClick = { onContinueClicked }
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320
+)
+@Composable
+fun PreviewOnboardingScreen() {
+    JetpackStoreTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
